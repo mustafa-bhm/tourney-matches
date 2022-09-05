29be3afbb3ba9b99158065734ee484be4e88ad11
mustafa <mustafa.bhm3@gmail.com>
2022-09-04 20:33:46 -0600

    Refactoring app.js - PlayerList - matchList.js

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,12 +1,10 @@
-import playerData from "../data/playerData";
-import matchData from "../data/matchData";
+// import playerData from "../data/playerData";
+// import matchData from "../data/matchData";
 import Player from "./Player";
-import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
+// import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-  const onePlayer = parsedPlayerData[0];
+  const onePlayer = props.playerData[0];
   return (
     <section className="PlayerList">
       <h1> Current Participating players</h1>
