2c4cecd7041106027972734adf0b7688af0a4b0c
mustafa <mustafa.bhm3@gmail.com>
2022-09-04 19:43:42 -0600

    conditional rendering Match component

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,3 +1,46 @@
+// function Match(props) {
+//   if (props.winner) {
+//     return (
+//       <article className="Match">
+//         <h1>
+//           {props.players[0]} <span>vs</span> {props.players[1]}
+//         </h1>
+//         {/* To be shown when there is a winner */}
+
+//         <h2>
+//           {props.winner} is the winner by {props.scoreDifference}
+//         </h2>
+//       </article>
+//     );
+//   } else {
+//     return (
+//       <article className="Match">
+//         <h1>
+//           {props.players[0]} <span>vs</span> {props.players[1]}
+//         </h1>
+
+//         {/* To be shown when there is no winner */}
+//         <h2>No winners yet!</h2>
+//       </article>
+//     );
+//   }
+
+//   // return (
+//   //   <article className="Match">
+//   //     <h1>
+//   //       {props.players[0]} <span>vs</span> {props.players[1]}
+//   //     </h1>
+//   //     {/* To be shown when there is a winner */}
+
+//   //     <h2>
+//   //       {props.winner} is the winner by {props.scoreDifference}
+//   //     </h2>
+//   //     {/* To be shown when there is no winner */}
+//   //     <h2>No winners yet!</h2>
+//   //   </article>
+//   // );
+// }
+
 function Match(props) {
   return (
     <article className="Match">
@@ -5,12 +48,15 @@ function Match(props) {
         {props.players[0]} <span>vs</span> {props.players[1]}
       </h1>
       {/* To be shown when there is a winner */}
+      {props.winner ? (
+        <h2>
+          {props.winner} is the winner by {props.scoreDifference} !
+        </h2>
+      ) : (
+        <h2>No winners yet!</h2>
+      )}
 
-      <h2>
-        {props.winner} is the winner by {props.scoreDifference}
-      </h2>
       {/* To be shown when there is no winner */}
-      <h2>No winners yet!</h2>
     </article>
   );
 }
