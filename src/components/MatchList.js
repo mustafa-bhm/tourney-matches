6d1c7eef7528684a1001b1068b3cfaf2710a6036
mustafa <mustafa.bhm3@gmail.com>
2022-09-04 21:16:40 -0600

    Mapping matches & players

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -2,16 +2,23 @@
 import Match from "./Match";
 
 function MatchList(props) {
-  const oneMatch = props.matchData[0];
+  // const oneMatch = props.matchData[0];
+  const matches = props.matchData.map((match) => {
+    return (
+      <Match
+        key={match.matchNumber}
+        players={match.players}
+        winner={match.winner}
+        scoreDifference={match.scoreDifference}
+      />
+    );
+  });
+
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
       {/* Matches will be shown here */}
-      <Match
-        players={oneMatch.players}
-        winner={oneMatch.winner}
-        scoreDifference={oneMatch.scoreDifference}
-      />
+      {matches}
     </section>
   );
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -4,16 +4,22 @@ import Player from "./Player";
 // import { preparePlayerData, addWinsToPlayers } from "../helpers/playerHelpers";
 
 function PlayerList(props) {
-  const onePlayer = props.playerData[0];
+  // const onePlayer = props.playerData[0];
+  const players = props.playerData.map((player) => {
+    return (
+      <Player
+        key={player.gamerTag}
+        firstName={player.firstName}
+        gamerTag={player.gamerTag}
+        lastName={player.lastName}
+        wins={player.wins}
+      />
+    );
+  });
   return (
     <section className="PlayerList">
       <h1> Current Participating players</h1>
-      <Player
-        firstName={onePlayer.firstName}
-        gamerTag={onePlayer.gamerTag}
-        lastName={onePlayer.lastName}
-        wins={onePlayer.wins}
-      />
+      {players}
     </section>
   );
 }
